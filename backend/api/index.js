1c6fcc6ab5f23c0adb018990a06d7494338277a6
hanzalasarwar <hanzala.sarwar.dev@gmail.com>
2026-05-11 15:16:16 +0500

    improving backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,9 @@
 
 import express from "express";
 import dotenv from "dotenv";
-import databaseConnection from "./utils/database.js";
+import databaseConnection from "../utils/database.js";
 import cookieParser from "cookie-parser";
-import userRoute from "./routes/userRoute.js";
+import userRoute from "../routes/userRoute.js";
 import cors from "cors";
 
 
